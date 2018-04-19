63479d0177c6a57fc7d7d0dc85158a6583a522e6
Nikolay Naidenov <nikolay@naidenov.me>
2018-04-19 17:10:12 +0300

    Auth in progress

---
+++ eshop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyActyLYqaEKU8hBUu22O48A25XWSnx_hUM',
+    authDomain: 'eshop-a7fb1.firebaseapp.com',
+    databaseURL: 'https://eshop-a7fb1.firebaseio.com',
+    projectId: 'eshop-a7fb1',
+    storageBucket: '',
+    messagingSenderId: '286940868614'
+  }
+};
